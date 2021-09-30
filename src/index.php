29b44f1f07e34d97e7a9b542a20a5de4c4c1e85a
Yanis <yanis@netkoders.com>
2021-09-30 16:14:30 +0200

    ouuo

--- src/index.php
+++ src/index.php
@@ -3,6 +3,8 @@ require_once '../vendor/autoload.php';
 
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
+use Twig\Loader\FilesystemLoader;
+use Twig\Environment;
 
 $logger = new Logger('main');
 
@@ -11,4 +13,12 @@ $logger->pushHandler(new StreamHandler(__DIR__.'/../log/app.log', Logger::DEBUG)
 $logger->info('First message');
 $logger->info('Second message');
 
-print("ok");
+print("1) ok</br>");
+
+$loader = new FilesystemLoader('../templates');
+
+print("2) ok</br>");
+
+$twig = new Environment($loader, ['cache' => '../cache']);
+
+print("3) ok</br>");
